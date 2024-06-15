1b7c710d7ba14bf252bf208543128fb0e120e053
jt10182000 <jotoquero.chmsu@gmail.com>
2024-06-16 05:27:36 +0800

    Update classification_src.py

--- My-streamlit-main/pages/classification_src.py
+++ My-streamlit-main/pages/classification_src.py
@@ -17,7 +17,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
-st.title("Fruit Identification")
+st.title("Fruit Classification")
 st.header("Supported fruits: lemon, apple, mandarin, orange")
 st.text("Upload a clear image of a fruit")
 
